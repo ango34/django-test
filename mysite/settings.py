de5c5e813eb1507f55ebe5544a52a7de9d0a31ad
ango34 <37031406+ango34@users.noreply.github.com>
2018-03-04 12:36:39 -0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,14 +76,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-##        'ENGINE': 'django.db.backends.sqlite3',
-##        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'defaultuser',
-        'PASSWORD': 'defaultuser',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
 }
 
